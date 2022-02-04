7377d16d3f79c649df40b55bb0fea505ea54ec44
Arshed Ahmed <arshedahmedmail@gmail.com>
2022-02-05 04:49:31 +0530

    detail pages

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,3 +1,4 @@
+import { MongoClient, ObjectId } from "mongodb";
 import MeetupDetail from "../../components/meetups/MeetupDetail.js";
 
 function MeetupDetails(props) {
@@ -13,36 +14,46 @@ function MeetupDetails(props) {
 }
 
 export async function getStaticPaths() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://admin:1234@cluster0.udioe.mongodb.net/Meetups?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+
+  const meetupCollection = db.collection("meetups");
+  const meetups = await meetupCollection.find({}, { _id: 1 }).toArray();
+  client.close();
+
   return {
     fallback: false,
-    paths: [
-      {
-        params: {
-          meetupId: "m1",
-        },
-      },
-      {
-        params: {
-          meetupId: "m2",
-        },
-      },
-    ],
+    paths: meetups.map((meetup) => ({
+      params: { meetupId: meetup._id.toString() },
+    })),
   };
 }
 
 export async function getStaticProps(context) {
   const meetupId = context.params.meetupId;
-  console.log(meetupId);
+
+  const client = await MongoClient.connect(
+    "mongodb+srv://admin:1234@cluster0.udioe.mongodb.net/Meetups?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+
+  const meetupCollection = db.collection("meetups");
+  const selectedMeetup = await meetupCollection.findOne({
+    _id: ObjectId(meetupId),
+  });
+  console.log(selectedMeetup);
+  client.close();
 
   return {
     props: {
       meetupData: {
-        image:
-          "https://live.staticflickr.com/4423/36745430904_114da4a4cb_b.jpg",
-        id: meetupId,
-        title: "First Meetup",
-        address: "Some street 5, Some city",
-        description: "This is a first meetup",
+        id: selectedMeetup._id.toString(),
+        image: selectedMeetup.image,
+        title: selectedMeetup.title,
+        address: selectedMeetup.address,
+        description: selectedMeetup.description,
       },
     },
   };
